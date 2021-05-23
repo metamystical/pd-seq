5d86ff207c85971420b6bf38f136cc0a56cf101a
Metamystical <metamystical@users.noreply.github.com>
2021-05-23 04:13:58 +0300

    Adding reset, slow toggle

--- seq.c
+++ seq.c
@@ -4,18 +4,36 @@ t_class *seq_class;
 
 typedef struct seq {
   t_object x_obj;
-  t_inlet *in;
+  t_inlet *in_size;
+  t_inlet *in_slow;
   t_outlet *out;
+  int default_base;
+  int default_size;
   int base;
   int size;
+  int slow;
+  int index;
 } t_seq;
 
 void seq_bang (t_seq *x) {
-  for (int i = 0; i < x->size; i++) outlet_float(x->out, (float)(x->base + i));
+  if (x->slow == 0) {
+    for (int i = 0; i < x->size; i++) outlet_float(x->out, (float)(x->base + i));
+  }  
+  else {
+    outlet_float(x->out, (float)(x->base + x->index));
+    if (++x->index == x->size) x->index = 0;
+  }
+}
+
+void seq_reset (t_seq *x) {
+  x->base = x->default_base;
+  x->size = x->default_size;
+  x->index = 0;
 }
 
 void seq_base (t_seq *x, t_floatarg f) {
   x->base = (int)f;
+  x->index = 0;
 }
 
 void seq_checkSize (t_seq *x) {
@@ -28,21 +46,32 @@ void seq_checkSize (t_seq *x) {
 void seq_size (t_seq *x, t_floatarg f) {
   x->size = (int)f;
   seq_checkSize(x);
+  x->index = 0;
+}
+
+void seq_slow (t_seq *x, t_floatarg f) {
+  x->slow = (int)f;
+  x->index = 0;
 }
 
 void *seq_new (t_floatarg base, t_floatarg size) {
   t_seq *x = (t_seq *)pd_new(seq_class);
   // leftmost inlet automatically created (and freed)
-  x->in = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("size"));
+  x->in_size = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("size"));
+  x->in_slow = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("slow"));
   x->out = outlet_new(&x->x_obj, &s_float);
-  x->base = (int)base;
-  x->size = (int)size;
+  x->slow = 0;
+  x->index = 0;
+  x->default_base = (int)base;
+  x->default_size = (int)size;
+  seq_reset(x);
   seq_checkSize(x);
   return (void *)x;
 }
 
 void seq_free(t_seq *x) {
-  inlet_free(x->in);
+  inlet_free(x->in_size);
+  inlet_free(x->in_slow);
   outlet_free(x->out);
 }
 
@@ -51,5 +80,7 @@ void seq_setup(void) {
     sizeof(t_seq), CLASS_DEFAULT, A_DEFFLOAT, A_DEFFLOAT, 0);
   class_addbang(seq_class, (t_method)seq_bang);
   class_addfloat(seq_class, (t_method)seq_base);
+  class_addmethod(seq_class, (t_method)seq_reset, gensym("reset"), 0);
   class_addmethod(seq_class, (t_method)seq_size, gensym("size"), A_DEFFLOAT, 0);
+  class_addmethod(seq_class, (t_method)seq_slow, gensym("slow"), A_DEFFLOAT, 0);
 }
